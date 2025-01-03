c78025badda2e8affe7d03142e74c3d8d9e02f5c
Pradeep <pawarpradeep1707@gmail.com>
2025-01-03 16:26:41 +0530

    Gesture

--- lib/main.dart
+++ lib/main.dart
@@ -7,33 +7,34 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void usertapped() {
+    print('User tapped the container');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: GridView.builder(
-          itemCount: 64,
-          gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 8),
-          itemBuilder: (context, index) => Container(
-            color: Colors.deepPurple[200],
-            margin: EdgeInsets.all(2),
+        body: Center(
+          child: GestureDetector(
+            onTap: usertapped,
+            child: Container(
+              height: 200,
+              width: 200,
+              color: Colors.deepPurple,
+              child: Center(
+                child: Text(
+                  'Hello, Flutter!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
           ),
-        ),
-        appBar: AppBar(
-          title: Text(
-            'My First app',
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ),
-          backgroundColor: Colors.deepPurple[600],
-          leading: Icon(Icons.menu, color: Colors.white),
-          actions: [
-            IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.logout, color: Colors.white)),
-          ],
-          elevation: 0,
         ),
       ),
     );
